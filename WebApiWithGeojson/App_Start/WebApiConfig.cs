1a6a4949679b215eebbaca72354ab3f9d6f84351
bertt <btemme@hotmail.com>
2014-10-28 16:26:13 +0100

    first

---
+++ WebApiWithGeojson/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace WebApiWithGeojson
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+        }
+    }
+}
---
+++ WebApiWithGeojson/Controllers/GeometryController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GeoJSON.Net.Feature;
+
+namespace WebApiWithGeojson.Controllers
+{
+    public class GeometryController : ApiController
+    {
+        public HttpResponseMessage GetFirst()
+        {
+            // return 2 features as geojson
+            var f1 = GetFeature(52.4667, 4.75);
+            var f2 = GetFeature(52.5667, 4.75);
+            var features = new List<Feature>();
+            features.Add(f1);
+            features.Add(f2);
+            var fc = new FeatureCollection(features);
+            return Request.CreateResponse(HttpStatusCode.OK, fc);
+
+        }
+
+        private Feature GetFeature(double Latitude, double Longitude)
+        {
+            var point = new GeoJSON.Net.Geometry.Point(new GeoJSON.Net.Geometry.GeographicPosition(Latitude, Longitude));
+            var featureProperties = new Dictionary<string, object> { { "Name", "Foo" } };
+            var feature = new Feature(point, featureProperties) { Id = "1" };
+            return feature;
+        }
+
+    }
+}
